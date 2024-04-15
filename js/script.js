dab6a6b21d74a45500726acc5c58f2362ba61720
Chris Clothier <c.r.clothier@gmail.com>
2024-04-15 11:58:25 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const xAxis = 16;
+const yAxis = 16;
+
+const container = document.querySelector(".container");
+const yAxisContainer = document.createElement("div");
+yAxisContainer.className = "yAxisContainer";
+const box = document.createElement("div");
+box.className = "box";
+
+container.appendChild(yAxisContainer);
+container.appendChild(box);
